2c83cc73ca422255b48dd2eae18112b704e3f4ce
Kuro <dev_kuro@yahoo.com>
2021-10-26 17:41:37 +0200

    updated test

--- src/test/java/kurodev/reader/UnknownSettingsIniParsing.java
+++ src/test/java/kurodev/reader/UnknownSettingsIniParsing.java
@@ -8,6 +8,17 @@ import java.nio.file.Path;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * [section1]
+ * setting1 = iniValue
+ * [iniSection](section3)
+ * hasWorked = true
+ * key1 = value1
+ * key2 = value2
+ * [section3](section1)
+ * setting2=differentValue
+ * setting1=overridden
+ */
 class UnknownSettingsIniParsing {
     Path iniFile = Path.of("./testResources/inherited.ini");
 
@@ -16,6 +27,8 @@ class UnknownSettingsIniParsing {
         var in = Files.newInputStream(iniFile);
         var instance = IniInstance.newInstance(in);
         assertTrue(instance instanceof UnknownSettingsImpl);
-        assertEquals(5, instance.getSection("iniSection").size());
+        assertEquals(6, instance.getSection("iniSection").size());
+        assertEquals("iniValue", instance.getSetting("setting1", "setting1"));
+        assertEquals("overridden", instance.getSetting("section3", "setting1"));
     }
 }
